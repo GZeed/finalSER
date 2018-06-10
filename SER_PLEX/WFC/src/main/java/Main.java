77b72ebe11397ce110806044db4d286c400bca4d
GZeed <guillaume.zaretti@heig-vd.ch>
2018-06-10 21:37:54 +0200

    main class ok

--- SER_PLEX/WFC/src/main/java/Main.java
+++ SER_PLEX/WFC/src/main/java/Main.java
@@ -1,11 +1,19 @@
 
+import java.io.Serializable;
+import java.rmi.RemoteException;
+import java.sql.Wrapper;
+import java.util.Observable;
+import java.util.Observer;
 import java.util.Scanner;
 
+import ch.heigvd.iict.ser.rmi.IClientApi;
+import ch.heigvd.iict.ser.rmi.IServerApi;
 import db.MySQLAccess;
 import ch.heigvd.iict.ser.imdb.models.Data;
 
-public class Main {
+public class Main extends Observable implements IServerApi {
 
+	private IServerApi serverApi = null;
 	static {
 		// this will load the MySQL driver, each DB has its own driver
 		try {
@@ -49,9 +57,77 @@ public class Main {
 				this.lastData = worker.run();
 
 				//TODO notify client
+				if(lastData != null){
+					setChanged();
+					notifyObservers(lastData);
+					System.out.println("new observers updated");
+				}
 				
 			}
 		}
 	}
 
+	/**
+	 * Method used by clients to register on the server
+	 *
+	 * @param client The client
+	 * @throws RemoteException
+	 */
+	@Override
+	public void addObserver(IClientApi client) throws RemoteException {
+		WrappedObserver wrappedObserver = new WrappedObserver(client);
+		addObserver(wrappedObserver);
+		System.out.println("wrappedObserver added");
+	}
+
+	/**
+	 * Method used by clients to check the connection with the server
+	 *
+	 * @return true is the server is reachable
+	 * @throws RemoteException
+	 */
+	@Override
+	public boolean isStillConnected() throws RemoteException {
+		return serverApi != null;
+	}
+
+	/**
+	 * Method used by clients to get all the data
+	 *
+	 * @return The data
+	 * @throws RemoteException
+	 */
+	@Override
+	public Data getData() throws RemoteException {
+		return lastData;
+	}
+
+	private class WrappedObserver implements Observer, Serializable{
+		private static final long serialVersionUID = -2067345842536415833L;
+
+		IClientApi clientApi = null;
+
+		public WrappedObserver(IClientApi clientApi) {
+			this.clientApi = clientApi;
+		}
+
+		/**
+		 * This method is called whenever the observed object is changed. An
+		 * application calls an <tt>Observable</tt> object's
+		 * <code>notifyObservers</code> method to have all the object's
+		 * observers notified of the change.
+		 *
+		 * @param o   the observable object.
+		 * @param arg an argument passed to the <code>notifyObservers</code>
+		 */
+		@Override
+		public void update(Observable o, Object arg) {
+			try{
+				clientApi.update(o.toString(), IClientApi.Signal.UPDATE_REQUESTED, arg.toString());
+			}catch(RemoteException e){
+				System.out.println("Remote exception removing observer : " + this);
+				o.deleteObserver(this);
+			}
+		}
+	}
 }
