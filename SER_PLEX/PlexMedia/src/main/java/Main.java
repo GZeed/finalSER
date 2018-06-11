740014d775f7925ecfcd7f1fb0feec76edb6f26d
GZeed <guillaume.zaretti@heig-vd.ch>
2018-06-11 13:11:11 +0200

    add dependencies interfaces

--- SER_PLEX/PlexMedia/src/main/java/Main.java
+++ SER_PLEX/PlexMedia/src/main/java/Main.java
@@ -1,4 +1,8 @@
+import ch.heigvd.iict.ser.rmi.IClientApi;
+
 import java.io.Console;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
 
 public class Main {
     public static void main(String[] args){
@@ -6,5 +10,24 @@ public class Main {
 
     }
 
-    private
+    private class RmiClient extends UnicastRemoteObject implements IClientApi {
+
+
+        /**
+         * Creates and exports a new UnicastRemoteObject object using an
+         * anonymous port.
+         * <p>
+         * <p>The object is exported with a server socket
+         * created using the {@link RMISocketFactory} class.
+         *
+         * @throws RemoteException if failed to export object
+         * @since JDK1.1
+         */
+        protected RmiClient() throws RemoteException {
+        }
+
+        public void update(Object observable, Signal signalType, String updateMsg) throws RemoteException {
+            
+        }
+    }
 }
